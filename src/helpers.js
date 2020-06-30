8d9d081ff104354dc96f70c1c8281f560d454fbb
Ahmad Hesam <hesam212@gmail.com>
2020-07-01 00:49:01 +0200

    Try different shell exec function

--- src/helpers.js
+++ src/helpers.js
@@ -1,23 +1,10 @@
-import { spawn } from 'child_process'
+const shell = require('shelljs');
 
 export const execShellCommand = (cmd) => {
   return new Promise((resolve, reject) => {
-    const process = spawn(cmd, [], { shell: true })
-    let stdout = ""
-    process.stdout.on('data', (data) => {
-      console.log(data.toString());
-      stdout += data.toString();
-    });
-
-    process.stderr.on('data', (data) => {
-      console.error(data.toString());
-    });
-
-    process.on('exit', (code) => {
-      if (code !== 0) {
-        reject(new Error(code ? code.toString() : undefined))
-      }
-      resolve(stdout)
+    shell.exec(cmd, (error, stdout, stderr) => {
+      if (error) console.warn(error);
+      resolve(stdout ? stdout : stderr);
     });
   });
 }
